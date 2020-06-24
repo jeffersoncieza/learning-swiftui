4d94a96517db2ce98389f87b24a4c6e238829fbf
Jeferson Cieza <jefferson.cieza@pucp.edu.pe>
2020-06-24 02:08:38 -0500

    Initial Commit

---
+++ learning-swiftui-1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  learning-swiftui-1
+//
+//  Created by Jeferson Cieza on 6/24/20.
+//  Copyright © 2020 Jeferson Cieza. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
